6feffc6bbe77456c31e5287d2acc4f81feeaea03
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-31 15:45:58 +0530

    validation model for user signup

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const router = require('./router')
 
-app.use(express.static('public'))
+app.use(express.static('public')) //import our public folder for static files
+
+app.use(express.urlencoded({extended: false})) //tell express to accept html form submits
+app.use(express.json()) //tell express to accept json data
 
 app.set('views', 'views')
 app.set('view engine', 'ejs')
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const validator = require('validator')
+
+let User = function(data) {
+    this.formData = data
+    this.errors = []
+}
+
+User.prototype.cleanUp = function() {
+    if (typeof(this.formData.username) != 'string') {this.formData.username = ""}
+    if (typeof(this.formData.email) != 'string') {this.formData.email = ""}
+    if (typeof(this.formData.password) != 'string') {this.formData.password = ""}
+
+    // purify unwanted inputs
+    this.formData = {
+        username: this.formData.username.trim().toLowerCase(),
+        email: this.formData.email.trim().toLowerCase(),
+        password: this.formData.password
+    }
+}
+
+User.prototype.validate = function() {
+
+    if (this.formData.username == ""){this.errors.push("You must provide a username!")}
+    else if (this.formData.username.length > 0 && this.formData.username.length < 3) {this.errors.push("Username should be atleast 3 characters!")}
+    else if (!validator.isAlphanumeric(this.formData.username)) {this.errors.push("Username can only contain letters and numbers!")}
+    else if (this.formData.username.length > 30) {this.errors.push("Username cannot exceed 30 characters!")}
+
+    if (!validator.isEmail(this.formData.email)){this.errors.push("You must provide a valid email address!")}
+
+    if (this.formData.password == ""){this.errors.push("You must provide a password!")}
+    else if (this.formData.password.length > 0 && this.formData.password.length < 8) {this.errors.push("Password should be atleast 8 characters!")}
+    else if (this.formData.password.length > 100) {this.errors.push("Password cannot exceed 100 characters!")}
+
+}
+
+User.prototype.register = function() {
+    // validate data
+    this.cleanUp()
+    this.validate()
+    
+    //iff no validation error save user data into database
+}
+
+module.exports = User
