dfe13667bd394a817c06954feaa01c89049231d8
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-31 17:25:46 +0530

    secured environment variables

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const mongodb = require('mongodb')
 const mongoose = require('mongoose')
 
-const connectionString = "mongodb+srv://todoAppUser:dbp4ssw0rd@cluster0.hswst.mongodb.net/high5?retryWrites=true&w=majority"
-
-port = 3000
+const connectionString = process.env.CONNECTIONSTRING
+port = process.env.PORT
 
 mongoose.connect(connectionString, (err) => {
     if (err) throw err
@@ -11,10 +12,3 @@ mongoose.connect(connectionString, (err) => {
         const app = require('./app')
         app.listen(port)
     })
-
-// [depricated]
-// mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-//     module.exports = client.db()
-//     const app = require('./app')
-//     app.listen(3000)
-// })
