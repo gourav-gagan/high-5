5c05ad1f8710467aebdac41e2bd7ea3b3798f1b8
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-31 12:53:47 +0530

    improved organisation - router and controllers

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = function() {
+
+}
+
+exports.logout = function () {
+    
+}
+
+exports.register = function() {
+    
+}
+
+exports.home = (req, res) => {
+    res.render('home-guest')
+}
